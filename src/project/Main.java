353f648790f7e36935e1d7e410bc5152464c0c71
tkuy1 <thierry.kuy@lunatech.fr>
2019-05-26 19:58:41 +0200

    Modification du main

--- src/project/Main.java
+++ src/project/Main.java
@@ -5,14 +5,15 @@ import java.util.Optional;
 public class Main {
     public static void main(String[] args) {
         Graph graph = Parser.parseToGraph(args[0]);
-
         NodeMap nodeMap = Parser.parseToCities(args[0]);
         System.out.println("End of parse");
         //System.out.println(nodeMap);
-        Optional<ShortestPathFromOneVertex> astar = Graphs.astar(graph, 1, 46104, Graphs.getH(graph.numberOfVertices(), 1, nodeMap));
+        int source = Integer.parseInt(args[1]);
+        int dest = Integer.parseInt(args[2]);
+        Optional<ShortestPathFromOneVertex> astar = Graphs.astar(graph, source, dest, Graphs.getH(graph.numberOfVertices(), source, nodeMap));
         astar.ifPresent(shortestPathFromOneVertex -> shortestPathFromOneVertex.printShortestPathTo(46104));
         System.out.println("==============");
-        ShortestPathFromOneVertex dijkstra = Graphs.dijkstra(graph, 1);
-        dijkstra.printShortestPathTo(46104);
+        ShortestPathFromOneVertex dijkstra = Graphs.dijkstra(graph, source);
+        dijkstra.printShortestPathTo(dest);
     }
 }
